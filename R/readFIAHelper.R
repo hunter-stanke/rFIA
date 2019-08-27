2307ae8603537d5cbb493dd36c3d53946da68d06
Hunter Stanke <stankehu@msu.edu>
2019-08-27 14:03:49 -0400

    Integer64 bug fix in readFIA. Convert to double.

--- R/readFIAHelper.R
+++ R/readFIAHelper.R
@@ -1,11 +1,18 @@
 readFIAHelper1 <- function(x, dir, ...){
   # Read in and append each file to a list
   file <- fread(paste(dir, x, sep = ""), showProgress = FALSE, logical01 = FALSE, integer64 = 'double', ...)
-  # suppressMessages({suppressWarnings({
-  # f <- read_csv(paste(dir, x, sep = ""), progress = FALSE, col_type = cols(.default = col_character()), ...)
-  # f <- type_convert(f)
-  # })})
+
+  # We don't want data.table formats
   file <- as.data.frame(file)
+
+  ## Use a loop to avoid using bit64 package (integer64 columns still appear for some reason)
+  classes <- sapply(file, class)
+  for (i in 1:ncol(file)){
+    if (classes[i] == 'integer64'){
+      file[,i] <- as.double(file[,i])
+    }
+  }
+
 
   file
 }
@@ -19,7 +26,16 @@ getFIAHelper <- function(x, dir, ...){
     fwrite(x = file, file = paste0(dir, str_sub(x, 43, -1)), showProgress = FALSE)
   }
 
+  # We don't want data.table formats
   file <- as.data.frame(file)
+
+  ## Use a loop to avoid using bit64 package (integer64 columns still appear for some reason)
+  classes <- sapply(file, class)
+  for (i in 1:ncol(file)){
+    if (classes[i] == 'integer64'){
+      file[,i] <- as.double(file[,i])
+    }
+  }
 
   file
 }
