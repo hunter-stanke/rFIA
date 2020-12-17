cadd4a04905a477cc865c7ee91fb5d2a747a5bd5
stankehu <stankehu@msu.edu>
2020-12-17 11:35:46 -0500

    Fixing per tree ratio estimates in vitalRates. Major bug affecting only these ratios in v0.3.0 and earlier.

--- R/globalVars.R
+++ R/globalVars.R
@@ -242,6 +242,7 @@ utils::globalVariables(c('ADJ_FACTOR_MACR', 'ADJ_FACTOR_MICR', 'ADJ_FACTOR_SUBP'
                          'lower_fe_int', 'lower_fe_rate', 'lower_int', 'lower_rate', 'upper_fe_int', 'fixed_rate',
                          'upper_fe_rate', 'upper_int', 'upper_rate', 'keep', 'nplts', 'p2eu_INVYR', 'fixed_alpha',
                          'PERC_AREA_VAR', 'aCV', 'acv', 'at', 'atEst', 'atStrat', 'atVar', 'atv', 'good',
-                         'REMPER_VAR', 'cvEst_remp', 'cvStrat_remp', 'rempEst', 'rempStrat', 'rempVar', 'rempv'
+                         'REMPER_VAR', 'cvEst_remp', 'cvStrat_remp', 'rempEst', 'rempStrat', 'rempVar', 'rempv',
+                         'bio'
 
 ))
